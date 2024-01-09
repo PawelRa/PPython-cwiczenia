6e030d84d3e680c3c491de56de07dd40ebe22b28
PawelRa <pawelrat@poczta.onet.pl>
2024-01-09 18:04:53 +0100

    użycie glob

--- zmiana_nazwy_pliku.py
+++ zmiana_nazwy_pliku.py
@@ -1,8 +1,14 @@
-NEW_EXTENSION = ".bak"
-file_name = input("Podaj nazwę pliku: ")
+import glob
 
-if '.' in file_name:
-    file_parts = file_name.rsplit('.', maxsplit=1)
-    print(file_parts[0] + NEW_EXTENSION)
-else:
-    print(file_name)
+NEW_EXTENSION = ".bak"
+file_path = input("Podaj ścieżkę: ")
+file_path += r"\*.txt"
+
+files = glob.glob(file_path)
+for file in files:
+    if '.' in file:
+        file_parts = file.rsplit('.', maxsplit=1)
+        file_path_parts = file_parts[0].rsplit("\\", maxsplit=1)
+        print("zamiana ", file, "na", file_path_parts[1] + NEW_EXTENSION)
+    else:
+        print(file)
