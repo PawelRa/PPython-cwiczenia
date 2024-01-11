445d8930a84fc553c0e04e0812b79bff54f1b3ee
PawelRa <pawelrat@poczta.onet.pl>
2024-01-11 18:34:11 +0100

    append

--- lista_w_wierszach.py
+++ lista_w_wierszach.py
@@ -1,14 +1,23 @@
-file_path = "d:\Kodowanie na githubie\PPython-cwiczenia\pliki\expenses_ex.txt"
+file_path = r"d:\Kodowanie na githubie\PPython-cwiczenia\pliki\expenses_ex.txt"
 
 with open(file_path) as stream:
     content = stream.read()
 
 expenses_list = content.split(sep='\n')
 
-result = 0
+expenses = []
 for linia in expenses_list:
     if linia:
         expense = linia.split(maxsplit=1)
-        result += float(expense[0])
+        expenses.append(float(expense[0]))
 
-print("Wydatki wynoszą -", result)
+suma = sum(expenses)
+minimum = min(expenses)
+maximum = max(expenses)
+average_cost = suma / len(expenses)
+
+
+print("Wszystkie wydatki wynoszą -", suma)
+print("Najniższy wydatek =", minimum)
+print("Najwyższy wydatek =", maximum)
+print("Średni wydatek =", average_cost)
