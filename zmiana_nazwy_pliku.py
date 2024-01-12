60c1a23d9a66aae84b54497c4fd0671dc9fe89b9
PawelRa <pawelrat@poczta.onet.pl>
2024-01-12 19:31:31 +0100

    zmiana nazw plików z potwierdzeniem

--- zmiana_nazwy_pliku.py
+++ zmiana_nazwy_pliku.py
@@ -6,10 +6,24 @@ file_path = input("Podaj ścieżkę: ")
 file_path += r"\*.txt"
 
 files = glob.glob(file_path)
+
+file_names = []
 for file in files:
     if '.' in file:
         file_name, extension = file.rsplit('.', maxsplit=1)
-        os.rename(file, file_name + NEW_EXTENSION)
-        print("Nowy plik =", file_name + NEW_EXTENSION)
+        file_names.append([file, file_name + NEW_EXTENSION])
     else:
         print(file)
+
+print("Czy chcesz zmienić poniższe pliki?")
+for file in file_names:
+    print(file[0], "--->", file[1])
+
+test = input("T/N ")
+
+if (test.lower() == 't'):
+    for old, new in file_names:
+        os.rename(old, new)
+    print("Nazwy zostały zmienione")
+else:
+    print("Anulowano zmiany")
